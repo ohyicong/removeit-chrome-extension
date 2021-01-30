9039aa3c98fb2205b67afa68595545b6e34314bd
ohyicong <ohyicong@hotmail.com>
2021-01-30 21:35:20 +0800

    Update with comments

--- content.js
+++ content.js
@@ -1,8 +1,11 @@
 var element = null;
 var element_background_color = null;
 var removed_elements = [];
+var websites = [];
 var is_activated = false;
 var is_shown = false;
+
+//highlight element when ctrl+alt+mouseover
 $("body").mouseover(function (event) {
   if (element) {
     $(element).css("background-color", element_background_color);
@@ -19,34 +22,78 @@ $("body").mouseover(function (event) {
     is_shown = false;
   }
 });
+//delete and save element when ctrl+alt+click
 $("body").click(function (event) {
+  is_website_found = false;
   if (event.ctrlKey && event.altKey) {
-    event.returnValue = "";
+    //disable element from performing default actions
     event.preventDefault();
     event.stopPropagation();
+    //delete element
     $(event.target).hide(1000);
+    //save deleted element
     removed_elements.push(structureSelector(event.target));
-    console.log(removed_elements);
-    chrome.storage.local.set(
-      { removed_elements: JSON.stringify(removed_elements) },
-      function () {
-        console.log("Value is set to:" + JSON.stringify(removed_elements));
+    for (var website_key in websites) {
+      if (websites[website_key]["website"] == window.location.href) {
+        websites[website_key]["removed_elements"] = removed_elements;
+        is_website_found = true;
       }
-    );
+    }
+    //if website not found, create new entry
+    if (!is_website_found) {
+      //check number of saved websites
+      console.log("websites", websites);
+      if (websites.length == 0) {
+        //initialisation
+        websites = [
+          {
+            website: window.location.href,
+            removed_elements: removed_elements,
+          },
+        ];
+      } else {
+        //subsequent entries
+        websites.push({
+          website: window.location.href,
+          removed_elements: removed_elements,
+        });
+      }
+    }
+    //save to chrome storage
+    chrome.storage.local.set({ websites: websites });
   }
 });
+
+//check chrome storage and delete all elements
 $(document).ready(function () {
-  chrome.storage.local.get(["removed_elements"], function (result) {
-    console.log(JSON.parse(result.removed_elements));
-    //removed elements from chrome local storage
-    let removed_elements = JSON.parse(result.removed_elements);
-    for (var key in removed_elements) {
-      console.log(removed_elements[key]);
-      $(removed_elements[key]).css("background-color", "#71AFE2");
-    }
-  });
+  setTimeout(function () {
+    //check chrome storage
+    chrome.storage.local.get("websites", function (result) {
+      websites = result.websites;
+      if (websites) {
+        console.log(websites);
+        for (var website_key in websites) {
+          console.log(websites[website_key]["website"]);
+          //check if website records exists
+          if (websites[website_key]["website"] == window.location.href) {
+            removed_elements = websites[website_key]["removed_elements"];
+            //remove elements
+            if (removed_elements) {
+              for (var removed_elements_key in removed_elements) {
+                $(removed_elements[removed_elements_key]).hide(1000);
+              }
+            }
+          }
+        }
+      } else {
+        websites = [];
+        console.log(websites);
+      }
+    });
+  }, 1000);
 });
 
+//create xpath for deleted element
 function structureSelector(element) {
   var tagName = element.tagName.toLowerCase();
   if (tagName == "body") {
