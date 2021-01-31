fcf8271c7d03ae257b5c25d0a93d620a00ad0446
ohyicong <ohyicong@hotmail.com>
2021-01-31 22:55:05 +0800

    bug fixed on notification

--- event.js
+++ event.js
@@ -5,6 +5,7 @@ var query = { active: true, currentWindow: true };
 //check if new tab is selected, update badge number
 chrome.tabs.onActivated.addListener(() => {
   chrome.tabs.query(query, (tabs) => {
+    var isWebsiteFound = false;
     tabUrl = tabs[0].url;
     console.log("active web: ", tabs[0].url);
     chrome.storage.local.get("websites", function (result) {
@@ -15,18 +16,21 @@ chrome.tabs.onActivated.addListener(() => {
           console.log(websites[websiteKey]["website"]);
           //check if website records exists
           if (websites[websiteKey]["website"] == tabUrl) {
+            isWebsiteFound = true;
             console.log("record exists");
             removedElements = websites[websiteKey]["removedElements"];
             numberOfRemovedElement = removedElements.length;
             //remove elements
             if (removedElements) {
+              console.log("event.js removed elements:", numberOfRemovedElement);
               chrome.browserAction.setBadgeText({
                 text: `${numberOfRemovedElement}`,
               });
             }
           }
         }
-      } else {
+      }
+      if (!isWebsiteFound) {
         console.log("record not exists 2");
         chrome.browserAction.setBadgeText({
           text: `0`,
@@ -39,6 +43,7 @@ chrome.tabs.onActivated.addListener(() => {
 //listen to any change in storage, update badge number
 chrome.storage.onChanged.addListener((result, storageName) => {
   chrome.tabs.query(query, (tabs) => {
+    var isWebsiteFound = false;
     tabUrl = tabs[0].url;
     console.log("active web: ", tabs[0].url);
     //alert(tabs[0].url);
@@ -49,6 +54,7 @@ chrome.storage.onChanged.addListener((result, storageName) => {
         console.log(websites[websiteKey]["website"]);
         //check if website records exists
         if (websites[websiteKey]["website"] == tabUrl) {
+          isWebsiteFound = true;
           removedElements = websites[websiteKey]["removedElements"];
           numberOfRemovedElement = removedElements.length;
           //remove elements
@@ -56,10 +62,16 @@ chrome.storage.onChanged.addListener((result, storageName) => {
             chrome.browserAction.setBadgeText({
               text: `${numberOfRemovedElement}`,
             });
+          } else {
+            chrome.browserAction.setBadgeText({
+              text: `0`,
+            });
           }
         }
       }
-    } else {
+    }
+    if (!isWebsiteFound) {
+      console.log("record not exists 2");
       chrome.browserAction.setBadgeText({
         text: `0`,
       });
--- popup.js
+++ popup.js
@@ -14,11 +14,13 @@ chrome.tabs.query(query, (tabs) => {
   console.log("active web: ", tabs[0].url);
   //check chrome storage
   chrome.storage.local.get("websites", function (result) {
+    var isWebsiteFound = false;
     websites = result.websites;
     if (websites) {
       console.log(websites);
       for (var websiteKey in websites) {
         console.log(websites[websiteKey]["website"]);
+        isWebsiteFound = true;
         //check if website records exists
         if (websites[websiteKey]["website"] == tabs[0].url) {
           removedElements = websites[websiteKey]["removedElements"];
@@ -29,7 +31,8 @@ chrome.tabs.query(query, (tabs) => {
           }
         }
       }
-    } else {
+    }
+    if (!isWebsiteFound) {
       $("#numberRemovedElements").text("0");
     }
   });
